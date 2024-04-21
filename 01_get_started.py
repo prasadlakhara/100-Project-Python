acbc43f03b6312b70bb750f7565ddd16fdbf4274
Prasad <prasadlakhara@gmail.com>
2024-04-21 15:25:22 +0530

    Get Started

---
+++ 01_get_started.py
@@ -0,0 +1,7 @@
+# Printing  Syntax 
+
+is_friend = True
+can_message = "message allowed" if is_friend else "Not Allowed"
+
+
+print(can_message)
---
+++ 02_data_types.py
@@ -0,0 +1,7 @@
+#                                                        Here are the Data Types of the Python
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 
+# Fundamental Data Types     ---------- 
+# 
+# int, float, bool, str, tuple, list, set, dict

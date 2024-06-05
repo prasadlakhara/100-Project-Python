ad40ac15d0bc051b0125e1461ec5ad6b02339529
Prasad <prasad.lakhara@eroam.com>
2024-06-05 14:24:40 +0530

    Removed unnecessary files

--- 01_get_started.py
+++
@@ -1,7 +0,0 @@
-# Printing  Syntax 
-
-is_friend = True
-can_message = "message allowed" if is_friend else "Not Allowed"
-
-
-print(can_message)
--- 02_data_types.py
+++
@@ -1,7 +0,0 @@
-#                                                        Here are the Data Types of the Python
-
-# ----------------------------------------------------------------------------------------------------------------------
-# 
-# Fundamental Data Types     ---------- 
-# 
-# int, float, bool, str, tuple, list, set, dict
